0278d3de2c3c7ae69fe2af5de7e0d706c11a0b34
Sohan Shrestha <sohanshrestha40@gmail.com>
2022-04-21 14:42:25 +0545

    codes

--- 2016-3-b.cpp
+++ 2016-3-b.cpp
@@ -1,5 +1,5 @@
 /*WAP to create a class EMP with data members(id,name,address).Derive a class SAlary with data members (basical,post) 
-and again derive class RECORD with data members(Month,Absent,Total) and display employee complete information whweere total is calculated
+and again derive class RECORD with data members(Month,Absent,Total) and display employee complete information where total is calculated
 by deducting "absent" days from "basical"*/
 #include<iostream>
 using namespace std;
@@ -39,6 +39,22 @@ class salary:public EMP
 };
 class record:public salary
 {
+	int absent,total,updated;
+	public:
+	static int month;
+	    void recData()
+	    {
+	    	cout<<"Absent Days:";
+	    	cin>>absent;
+	    	total=month-absent;
+	    	cout<<"Total:"<<total;
+		}
+		void calSal()
+		{
+			updated=basical/30;
+			basical=updated*total;
+		}
 	
 };
+int record::month=30
 
